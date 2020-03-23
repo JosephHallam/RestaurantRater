0a762ca5b97910d247420adb9aee6860fd492c8b
Joseph Hallam <joehallam13@gmail.com>
2020-03-23 15:32:39 -0400

    Added Edit Functionality

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -57,6 +58,34 @@ namespace RestaurantRater.Controllers
             _ctx.Restaurants.Remove(restaurant);
             _ctx.SaveChanges();
             return RedirectToAction("Index");
+        }
+        //GET : Restaurant/Edit/{id}
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var restaurant = _ctx.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        //POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant item)
+        {
+            if (ModelState.IsValid)
+            {
+                _ctx.Entry(item).State = EntityState.Modified;
+                _ctx.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(item);
         }
     }
 }
